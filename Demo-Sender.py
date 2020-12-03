c3c910494eec450131fc449e8287915dc4af7466
creator <noemail@cnowhere.universe>
2020-12-03 10:00:33 +0100

    genericised

--- Demo-Sender.py
+++ Demo-Sender.py
@@ -3,7 +3,7 @@ import IPFS_DataTransmission#_old as IPFS_DataTransmission
 data = "Hello IPFS World! New way of networking coming up. Can't wait to use it!".encode("utf-8")
 
 # Uncomment the following two lines of code to set the data to send a file
-with open("/home/elborth/Desktop/00.jpg", "rb") as conff:
-    data = conff.read()
+# with open("PASE YOUR PATH HERE", "rb") as conff:
+#     data = conff.read()
 
-IPFS_DataTransmission.TransmitData(data, "12D3KooWQzQAs2tCuY6P9V3qaKKars17JpaLkvEaHi5RBsUXVj94", "test application")
+IPFS_DataTransmission.TransmitData(data, "PASTE PEER ID HERE", "test application")
