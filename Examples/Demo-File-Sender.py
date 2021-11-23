fe8d168e59fa0fee63e34b0e84c72a0660ff070d
creator <noemail@cnowhere.universe>
2021-11-23 07:22:40 +0100

    made Demo-File-Sender a bit clearer on its usage of metadata

--- Examples/Demo-File-Sender.py
+++ Examples/Demo-File-Sender.py
@@ -18,9 +18,12 @@ peerID = "QmHash"
 # insert the path of the file to transmit here
 filepath = ""
 
+# you can send any metadata your like
+metadata = os.path.basename(filepath).encode()
+
 # making sure our IPFS node finds the receiver computer on the IP layer of the internet
 IPFS_API.FindPeer(peerID)
 
 # Transmit the file. The object ft can be referenced for example to check transmission progress
 ft = IPFS_DataTransmission.TransmitFile(
-    filepath, peerID, "my_apps_filelistener", os.path.basename(filepath).encode())
+    filepath, peerID, "my_apps_filelistener", metadata)
